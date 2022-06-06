d33a37a38d6e0d7f2cb7f8e3f481f45f6091e067
PhamThanhThien <ptthien124@gmail.com>
2022-06-06 10:23:29 +0700

    import react

--- imports/ui/components/login&signUp/Form.jsx
+++ imports/ui/components/login&signUp/Form.jsx
@@ -1,5 +1,5 @@
 import { Spin } from "antd";
-import { default as React, default as React, useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { loginRequest } from "../../redux/actions/authAction";
